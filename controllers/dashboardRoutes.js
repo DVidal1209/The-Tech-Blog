a44c05a29f6335f3cda59c123400f030aa2c2fda
Daniel Vidal <daniel.vidal17@yahoo.com>
2022-11-23 16:05:55 -0500

    created all Routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,10 +1,34 @@
-const withAuth = require('../utils/auth');
 const router = require('express').Router();
 const { Blog, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 
-router.get('/', (req, res) => {
-    
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const blogData = Blog.findAll({include: {model: User, attributes: username}},{
+            where: {
+                user_id: req.session.user_id,
+            }
+        })
+        const blogs = (await blogData).map((blog) => blog.get({ plain: true }))
+
+        res.status(200).render('dashboard', { blogs });
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/blogspot/:id', withAuth, async (req, res) => {
+    try{
+        const blogData = Blog.findByPk({id: req.params.id})
+        const blog = (await blogData).map((blogInfo) => blogInfo.get({ plain: true }))
+        
+        res.status(200).render('userBlog', {blog});
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,15 @@
 const router = require('express').Router();
 const { Blog, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
 
     const blogData = await Blog.findAll({
-      include: [
+      include: 
         {
           model: User,
-          attributes: ['username'],
+          attributes: 'username',
         },
-      ],
     });
 
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
