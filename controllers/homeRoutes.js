fbad142993bbb37c9eaa30d1ed597b83490a50a2
Daniel Vidal <daniel.vidal17@yahoo.com>
2022-11-23 13:48:34 -0500

    Added homepage route to homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,27 @@
 const router = require('express').Router();
+const { Blog } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try{
+
+    const blogData = await Project.findAll({
+      include: [
+        {
+          model: Blog,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+
+    res.render('homepage', {blogs, logged_in: req.session.logged_in})
+  }
+  catch (err){
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
